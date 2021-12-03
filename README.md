2617819034e7957b131d2ad6bc5b2f0220569fb5
mulcmu <mulcmu@yahoo.com>
2021-12-03 09:14:22 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # esphome-marlin-uart
- Custom ESPhome device
+ Work in progress for integrating a FLSUN QQ-S Pro printer with home assistant using ESPhome.  The printer main board includes a ESP8266 module connected to one of the serial outputs.  Stock printer firmware has been replaced with Marlin 2.x.
+
+Temperature reports and printer status currently implemented.
+
+For initial installation follow the https://github.com/Foxies-CSTL/Marlin_2.0.x/wiki/5.Firmware-Wifi guide to flash the MksWifi.bin.  You will need to build the firmware in ESPhome and rename the bin file to MksWifi.bin.  After initial installation OTA updates should work fine.
+
+TODO:
+
+- Thermal runaway / halt status added.
+- Elapsed print time
+- Estimated remaining time
+- Service to preheat nozzle/bed
+- Upload file to SD card
+
---
+++ esphome-marlin-uart.h
@@ -0,0 +1,219 @@
+
+//class constructor based off of @mannkind
+//ESPHomeRoombaComponent/ESPHomeRoombaComponent.h
+//https://github.com/mannkind/ESPHomeRoombaComponent
+
+#include "esphome.h"
+
+#define PROGRESS_INTERVAL 15000 //15 seconds
+
+static const char *TAG = "component.MarlinUART";
+
+class component_MarlinUART : public PollingComponent, public UARTDevice {
+
+  public:
+    Sensor *sensor_bedtemp;
+    Sensor *sensor_bedsetpoint;
+    
+    Sensor *sensor_exttemp;
+    Sensor *sensor_extsetpoint;
+    
+    Sensor *sensor_progress;
+    
+    TextSensor *textsensor_printerState;
+
+    static component_MarlinUART* instance(UARTComponent *parent)
+    {
+        static component_MarlinUART* INSTANCE = new component_MarlinUART(parent);
+        return INSTANCE;
+    }
+
+    //delay setup() so that printer has booted before sending the M155 command.
+    float get_setup_priority() const override { 
+        return esphome::setup_priority::LATE; 
+    }
+
+    void setup() override
+    {
+        ESP_LOGD(TAG, "setup().");
+        
+        MarlinOutput.reserve(256);  //allocate hefty buffer for String object
+        MarlinOutput = "";
+        
+        flush();
+        write_str("\r\n\r\nM155 S10\r\n");  //Auto report temperatures every 10 seconds
+        
+        textsensor_printerState->publish_state("Unknown");
+        
+        sensor_progress->publish_state(NAN);
+        
+    }
+
+    void update() override
+    {
+        ESP_LOGV(TAG, "update().");
+
+        int16_t distance;
+        uint16_t voltage;
+        bool publishJson;
+        
+        while ( available() ) {
+            char c = read();
+            MarlinOutput += c;
+            if( c == '\n' || c == '\r' ) {
+                process_line();
+            }
+        }
+        
+        
+        if(millis() - millisProgress > PROGRESS_INTERVAL)  {
+            write_str("M27\r\n");
+            millisProgress = millis();
+        }
+        // Only publish new states if there was a change
+
+        /*
+        if (this->cleaningBinarySensor->state != cleaningState) {
+            this->cleaningBinarySensor->publish_state(cleaningState);
+        } 
+        */
+    }
+
+
+  private: 
+    String MarlinOutput;
+    String StateText;
+    unsigned long millisProgress=0;
+    
+
+    component_MarlinUART(UARTComponent *parent) : PollingComponent(2000), UARTDevice(parent) 
+    {
+       
+        this->sensor_bedtemp = new Sensor();
+        this->sensor_bedsetpoint = new Sensor();
+        
+        this->sensor_exttemp = new Sensor();
+        this->sensor_extsetpoint = new Sensor();
+        
+        this->sensor_progress = new Sensor();
+    
+        this->textsensor_printerState = new TextSensor();
+    }
+    
+  
+    void process_line() {
+        //char debugstring[256];
+        //MarlinOutput.toCharArray(debugstring,256);
+        
+        ESP_LOGD(TAG, MarlinOutput.c_str() );
+        
+        // Auto reported temperature
+        // T:157.35 /0.00 B:56.56 /0.00 @:0 B@:0
+        // T:19.57 /0.00 B:20.12 /0.00 @:0 B@:
+        if(MarlinOutput.startsWith(String(" T:"))) {
+
+            //Extruder temp
+            short start=3;
+            short end = MarlinOutput.indexOf('/');
+            if (end == -1) {
+                MarlinOutput="";
+                return;
+            }
+                
+            float t = MarlinOutput.substring(start,end).toFloat();
+            sensor_exttemp->publish_state(t);
+
+            //Extruder setpoint
+            start = end + 1;
+            end = MarlinOutput.indexOf(' ', start);
+            if (end == -1) {
+                MarlinOutput="";
+                return;
+            }
+            t = MarlinOutput.substring(start,end).toFloat();
+            sensor_extsetpoint->publish_state(t);
+            
+            //Bed temp
+            start = end + 3;
+            end = MarlinOutput.indexOf('/', start);
+            if (end == -1) {
+                MarlinOutput="";
+                return;
+            }
+            t = MarlinOutput.substring(start,end).toFloat();
+            sensor_bedtemp->publish_state(t);
+            
+            //Bed setpoint
+            start = end + 1;
+            end = MarlinOutput.indexOf(' ', start);
+            if (end == -1) {
+                MarlinOutput="";
+                return;
+            }
+            t = MarlinOutput.substring(start,end).toFloat();
+            sensor_bedsetpoint->publish_state(t);
+            
+            MarlinOutput="";
+            return;
+        }
+        
+        
+        
+        
+        //SD printing byte 2467546/3281364
+       if(MarlinOutput.startsWith(String("SD printing byte "))) {
+            long current, total;
+            
+            current = MarlinOutput.substring(17).toInt();
+            total = MarlinOutput.substring(MarlinOutput.indexOf('/')+1).toInt();
+            
+            ESP_LOGD(TAG,String(current).c_str());
+            ESP_LOGD(TAG,String(total).c_str());
+            
+            if (total==0)  {
+                sensor_progress->publish_state(NAN);
+                        }
+            else  {
+                sensor_progress->publish_state( (float) current / (float) total * 100.0);
+            }
+            
+            MarlinOutput="";
+            return;
+        } 
+        
+        
+        //State
+        //action:prompt_begin FilamentRunout T0
+        if(MarlinOutput.indexOf(String("FilamentRunout")) != -1) {
+            textsensor_printerState->publish_state("Filament Runout");
+            MarlinOutput="";
+            return;
+        }
+        
+        if(MarlinOutput.startsWith(String("echo:busy: paused"))) {
+            if(textsensor_printerState->state != "Filament Runout") {
+                textsensor_printerState->publish_state("Printing Paused");
+            }
+            MarlinOutput="";
+            return;
+        }
+                
+        if(MarlinOutput.startsWith(String("Done printing"))) {
+            textsensor_printerState->publish_state("Finished Printing");
+            sensor_progress->publish_state(NAN);
+            MarlinOutput="";
+            return;
+        }
+        
+        if(MarlinOutput.startsWith(String("echo:busy: processing"))) {
+            textsensor_printerState->publish_state("Printing");
+            MarlinOutput="";
+            return;
+        }
+        
+        
+        //reset string for next line
+        MarlinOutput="";
+    }
+};
+    
